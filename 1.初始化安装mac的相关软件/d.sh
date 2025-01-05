c6ffe013a2479cef11af3c0352aa2069cafc421a
MOJORU <34601389+mojoru2023@users.noreply.github.com>
2025-01-05 19:07:33 +0900

    Create d.sh

---
+++ 1.初始化安装mac的相关软件/d.sh
@@ -0,0 +1,38 @@
+
+
+# install homebrew 
+
+/bin/bash -c "$(curl -fsSL https://gitee.com/ineo6/homebrew-install/raw/master/install.sh)"
+
+
+# # 查看配置
+brew config
+# 更新 Homebrew
+brew update
+#  搜索相关的包
+brew search [关键词] 
+# 查看包的信息
+brew info [软件名]
+# 查看已安装的包
+brew list
+# 更新某个软件
+brew upgrade [软件名]
+# 清理所有软件的旧版
+brew cleanup
+# 卸载某个软件
+brew uninstall [软件名]
+# 安装某个软件
+brew install [软件名]
+# 安装cask软件
+brew install --cask [软件名]
+
+brew install zsh
+# install ：安装 Oh My Zsh 并配置
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# 自动补全插件
+brew install zsh-syntax-highlighting
+
+
+# install flutter 
+
+brew install --cask flutter
