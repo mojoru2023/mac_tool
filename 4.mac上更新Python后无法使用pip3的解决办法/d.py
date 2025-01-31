17f6a5636bf90efe913fefc249cee00380fa35ec
MOJORU <34601389+mojoru2023@users.noreply.github.com>
2025-01-31 13:43:08 +0900

    Create d.py

---
+++ 4.mac上更新Python后无法使用pip3的解决办法/d.py
@@ -0,0 +1,10 @@
+
+
+#参考 Mac更新python3.12 解决pip3 https://www.jb51.net/python/3205701ir.htm 
+
+要删除EXTERNALLY-MANAGED这个文件。
+
+在Mac系统里，这个文件藏在这里，删除即可。
+
+藏在下面的路径下
+/usr/local/Cellar/python@3.12/3.12.3/Frameworks/Python.framework/Versions/3.12/lib/python3.12
